6efedc15589c145bd1872373d8998f52f2b8f1a3
Devworld8 <chinnarasu.m@gmail.com>
2014-06-27 19:04:10 +0530

    Tablet and laptop complete

--- LineFocus.Nikcron/Controllers/ProductController.cs
+++ LineFocus.Nikcron/Controllers/ProductController.cs
@@ -53,9 +53,73 @@ namespace LineFocus.Nikcron.Controllers
             return Result;
         }
 
-        public ActionResult LaptopMaintenance()
+        [HttpGet]
+        public ActionResult LaptopMaintenance(Int32? LaptopId)
         {
-            return View();
+            ViewBag.Header = "Laptop";
+            if (LaptopId.HasValue)
+            {
+                ViewBag.LaptopId = LaptopId;
+                ViewBag.Caption = "Edit";
+                ApplicationDBContext db = new ApplicationDBContext();
+                Laptop loptop = db.ProductModels.OfType<Laptop>().Where(l => l.Id == LaptopId).FirstOrDefault();
+                return View(loptop);
+            }
+            else
+            {
+                ViewBag.LaptopId = 0;
+                ViewBag.Caption = "New";
+                return View(new Laptop());
+            }
+        }
+
+        [HttpPost]
+        public ActionResult LaptopMaintenance(FormCollection formCollection)
+        {
+            ApplicationDBContext db = new ApplicationDBContext();
+            Laptop Laptop = new Laptop();
+
+            Int32 LaptopId = 0;
+            Int32.TryParse(formCollection["Laptopid"], out LaptopId);
+            if (LaptopId == 0)
+                Laptop = new Laptop();
+            else
+
+            Laptop = db.ProductModels.OfType<Laptop>().Where(l => l.Id == LaptopId).FirstOrDefault();
+            Laptop.ProductTypeId = db.ProductTypes.Where(t => t.Name == "Laptop").FirstOrDefault().Id;
+            Laptop.ManufactureId = int.Parse(formCollection["Manufacture"]);            
+            Laptop.ModelName = formCollection["ModelName"];
+            Laptop.ModelNumber = formCollection["ModelNumber"];
+           // Laptop.ProductColors = formCollection["ProductColors"];
+            Laptop.Processor = formCollection["Processor"];
+            Laptop.Chipset = formCollection["Chipset"];
+            Laptop.ClockSpeed = formCollection["ClockSpeed"];
+            Laptop.Cache = formCollection["Cache"];
+            Laptop.ExternalStorage = formCollection["ExternalStorage"];
+            Laptop.HardwareInterface = formCollection["HardwareInterface"];
+            Laptop.RPM = formCollection["RPM"];
+            Laptop.HDDCapacity = formCollection["HDDCapacity"];
+            Laptop.OpticalDrive = formCollection["OpticalDrive"];
+            //Laptop.ProductWarranties = formCollection["ProductWarranties"];
+            Laptop.OperatingSystem = formCollection["OperatingSystem"];
+            Laptop.SystemArch = formCollection["SystemArch"];
+            Laptop.DisplaySize = formCollection["DisplaySize"];
+            Laptop.DisplayType = formCollection["DisplayType"];
+            Laptop.Keyboard = formCollection["Keyboard"];
+            Laptop.Speakers = formCollection["Speakers"];
+            Laptop.Sound = formCollection["Sound"];
+            Laptop.USBPort = formCollection["USBPort"];
+            Laptop.RJ45LAN = formCollection["RJ45LAN"];
+            Laptop.HDMIPort = formCollection["HDMIPort"];
+            Laptop.VGAPort = formCollection["VGAport"];
+            Laptop.MultiCardSlot = formCollection["MultiCardSlot"];
+            Laptop.Weight = formCollection["Weight"];
+            
+
+            if (LaptopId == 0)
+                db.ProductModels.AddObject(Laptop);
+            db.SaveChanges();
+            return View("Laptops");
         }
 
         public ActionResult Mobilephones(Int32? MobileId)
@@ -160,14 +224,92 @@ namespace LineFocus.Nikcron.Controllers
             db.SaveChanges();
             return View("Mobilephones");
         }
+        
         public ActionResult Tablets()
         {
             return View();
         }
 
-        public ActionResult TabletMaintenance()
+        public JsonResult GetTablets()
         {
-            return View();
+            JsonResult Result = new JsonResult();
+            ApplicationDBContext db = new ApplicationDBContext();
+            Result.Data = db.ProductModels.OfType<Tablet>().Select(n => new
+            {
+                n.Id,
+                n.ModelNumber,
+                Brand = n.Manufacture.Name,
+                OS = n.OperatingSystem,
+                HDD = n.InternalStorage,
+                RAM = n.Memory,
+                Processor = n.Processor,
+                Screen = n.DisplaySize
+            });
+            Result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return Result;
         }
+
+       [HttpGet]
+        public ActionResult TabletMaintenance(Int32? TabletId)
+        {
+            ViewBag.Header = "Tablet";
+            if (TabletId.HasValue)
+            {
+                ViewBag.TabletId = TabletId;
+                ViewBag.Caption = "Edit";
+                ApplicationDBContext db = new ApplicationDBContext();
+                Tablet Tablet = db.ProductModels.OfType<Tablet>().Where(t => t.Id == TabletId).FirstOrDefault();
+                return View(Tablet);
+            }
+            else
+            {
+                ViewBag.TabletId = 0;
+                ViewBag.Caption = "New";
+                return View(new Tablet());
+            }
+        }
+
+       [HttpPost]
+       public ActionResult TabletMaintenance(FormCollection formCollection)
+       {
+           ApplicationDBContext db = new ApplicationDBContext();
+           Tablet Tablet = new Tablet();
+
+           Int32 Tabletid = 0;
+           Int32.TryParse(formCollection["Tabletid"], out Tabletid);
+           if (Tabletid == 0)
+               Tablet = new Tablet();
+           else
+
+               Tablet = db.ProductModels.OfType<Tablet>().Where(t => t.Id == Tabletid).FirstOrDefault();
+           Tablet.ProductTypeId = db.ProductTypes.Where(t => t.Name == "Tablet").FirstOrDefault().Id;
+           Tablet.ManufactureId = int.Parse(formCollection["Manufacture"]);
+           Tablet.ModelName = formCollection["ModelName"];
+           Tablet.ModelNumber = formCollection["ModelNumber"];
+          
+           Tablet.Processor = formCollection["Processor"];
+          // Tablet.ProductColors = formCollection["ProductColors"];
+           Tablet.ExternalStorage = formCollection["ExternalStorage"];
+           Tablet.InternalStorage = formCollection["InternalStorage"];
+           Tablet.Dimension = formCollection["Dimension"];
+           Tablet.OperatingSystem = formCollection["OperatingSystem"];
+           Tablet.DisplaySize = formCollection["DisplaySize"];
+           Tablet.DisplayType = formCollection["DisplayType"];
+           Tablet.PrimaryCamera = formCollection["PrimaryCamera"];
+           Tablet.SecondryCamera = formCollection["SecondryCamera"];
+           Tablet.WiFi = formCollection["WiFi"];
+           //Tablet.USBPort = formCollection["USBPort"];
+           Tablet.PreinstalledBrowser = formCollection["PreinstalledBrowser"];
+           Tablet.Weight = formCollection["Weight"];
+           Tablet.VideoFormat = formCollection["VideoFormat"];
+           Tablet.VideoRecording = formCollection["VideoRecording"];
+           Tablet.AudioFormat = formCollection["AudioFormat"];
+
+           if (Tabletid == 0)
+               db.ProductModels.AddObject(Tablet);
+           db.SaveChanges();
+           return View("Tablets");
+       }
+
     }
 }
